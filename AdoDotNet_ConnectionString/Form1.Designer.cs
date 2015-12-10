3d1d7ff7f17e070a4b9a34b8b08983ffc6b14820
Faith Nielsen <faiteo2002@yahoo.com>
2015-12-10 11:49:25 +0100

    Connectionstring, Configuration file and Using block
    Ado.Net: ConnectionString inside a configuration file. Database
    connection inside a using block.

---
+++ AdoDotNet_ConnectionString/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace AdoDotNet_ConnectionString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgView = new System.Windows.Forms.DataGridView();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.lblErrorMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgView
+            // 
+            this.dgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgView.Location = new System.Drawing.Point(12, 12);
+            this.dgView.Name = "dgView";
+            this.dgView.Size = new System.Drawing.Size(440, 150);
+            this.dgView.TabIndex = 0;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(333, 168);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(119, 23);
+            this.btnLoadData.TabIndex = 1;
+            this.btnLoadData.Text = "Load Employee Data";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // lblErrorMessage
+            // 
+            this.lblErrorMessage.AutoSize = true;
+            this.lblErrorMessage.Location = new System.Drawing.Point(12, 218);
+            this.lblErrorMessage.Name = "lblErrorMessage";
+            this.lblErrorMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblErrorMessage.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 262);
+            this.Controls.Add(this.lblErrorMessage);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.dgView);
+            this.Name = "Form1";
+            this.Text = "Employees";
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgView;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.Label lblErrorMessage;
+    }
+}
+
---
+++ AdoDotNet_ConnectionString/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AdoDotNet_ConnectionString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CS1"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("Select * from Employee", con))
+                    {
+                        con.Open();
+                        SqlDataReader rdr = cmd.ExecuteReader();
+                        BindingSource source = new BindingSource();
+                        source.DataSource = rdr;
+                        dgView.DataSource = source;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblErrorMessage.ForeColor = System.Drawing.Color.Red;
+                lblErrorMessage.Text = "Could not get Employee data from database";
+                
+            }
+                
+                        
+
+        }
+    }
+}
